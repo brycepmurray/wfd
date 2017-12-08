ec97366411c7477287e221a815820c579439ebf3
Bryce Murray <brycepmurray711@gmail.com>
2017-12-08 10:11:44 -0700

    added bootstrap require to main.js

--- www/src/main.js
+++ www/src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+require('../node_modules/bootstrap/less/bootstrap.less')
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -9,17 +10,17 @@ import store from './store'
 
 let socket = io('http://localhost:3000')
 
-socket.on('CONNECTED', function (data) {
-  console.log(data)
-  socket.emit('update', { data: 'blarg', boardId: '3289748320' })
+socket.on('CONNECTED', function(data) {
+    console.log(data)
+    socket.emit('update', { data: 'blarg', boardId: '3289748320' })
 })
 
 
 
 new Vue({
-  el: '#app',
-  store,
-  router,
-  template: '<App/>',
-  components: { App }
+    el: '#app',
+    store,
+    router,
+    template: '<App/>',
+    components: { App }
 })
