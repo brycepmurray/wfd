78ed66a40545f5c7b4e002d2ff9c27a99eac5885
Bryce Murray <brycepmurray711@gmail.com>
2017-12-14 15:22:08 -0700

    calendar <a> tag added to navbar, but commented out for stretch goal functionality and creation

--- www/src/components/Navbar.vue
+++ www/src/components/Navbar.vue
@@ -11,6 +11,10 @@
       <li>
         <a href="#cookbook">My Recipes</a>
       </li>
+
+      <!-- <li>
+        <a href="#calendar">My Calendar</a>
+      </li> -->
       
       <li class="logout">
         <a @click="logout">Log Out</a>
@@ -23,55 +27,54 @@
 
 
 <script>
-  export default {
-    name: 'navbar',
-    data() {
-      return {
+    export default {
+        name: 'navbar',
+        data() {
+            return {
 
-      }
-    },
-    computed: {},
-    methods: {
-      logout() {
-        this.$store.dispatch('logout')
-      }
-    },
-    components: {}
-  }
+            }
+        },
+        computed: {},
+        methods: {
+            logout() {
+                this.$store.dispatch('logout')
+            }
+        },
+        components: {}
+    }
 </script>
 
 
 <style scoped>
-  ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: #333;
-  }
-
-  li {
-    float: left;
-  }
-  li a {
-    display: block;
-    color: white;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-  }
-
-
-  .logout {
-    float: right;
-  }
-
-  .logout:hover {
-    color: red;
-
-  }
-
-  li a:hover {
-    background-color: #111;
-  }
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #333;
+    }
+    
+    li {
+        float: left;
+    }
+    
+    li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+    
+    .logout {
+        float: right;
+    }
+    
+    .logout:hover {
+        color: red;
+    }
+    
+    li a:hover {
+        background-color: #111;
+    }
 </style>
