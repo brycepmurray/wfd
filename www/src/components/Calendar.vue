00a18a9571e33f941a0be6fda1b89b87cd60d99a
Bryce Murray <brycepmurray711@gmail.com>
2017-12-14 16:16:06 -0700

    date selector Form

--- www/src/components/Calendar.vue
+++ www/src/components/Calendar.vue
@@ -1,11 +1,15 @@
 <template>
     <!-- Adding a Date Setter so that user can select week that they would like to have displayed and selected -->
-        <div class="form-group">
+    <div>
+        <form>    
+            <div class="form-group">
                 <label for="startDate">Start Date:</label>
-                <input type="date" name="startDate" class="form-control" placeholder="Start Date" :min="date" v-model="event.startDate" required
-                    @change="validateForm">
+                <input type="date" id="week" name="week" class="form-control" placeholder="Start Date" :min="date" v-model="event.startDate" required
+                @change="validateForm">
                 <p class="error-message text-left text-danger" v-if="!this.validator.startDate">Start date must be today or later.</p>
-                
+            </div>        
+        </form>     
+    </div>
 </template>
 
 
