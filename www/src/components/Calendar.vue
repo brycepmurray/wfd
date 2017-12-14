86552471bfeaccb79000300781fd405a2a114d23
Your Name <Your email>
2017-12-14 11:20:55 -0700

    keep calendar,  puu all rest

--- www/src/components/Calendar.vue
+++ www/src/components/Calendar.vue
@@ -1,25 +1,38 @@
 <template>
-        <div>
-            <div> 
-    
-                <!-- Sidebar -->
-                <div id="sidebar">
-                    <!-- Sidebar Header -->
-                    <div class="sidebar-header row">
-                        <button type="button" id="sidebarCollapse" class="btn btn-info navbar-btn">
-                            <i class="glyphicon glyphicon-align-right"></i>
-                            
-                        </button>
-                    </div>
-    
-                    <!-- Sidebar Links -->
-                    <ul class="list-unstyled components">
+
+    <div>
+
+
+        <div class="container">
+            <div v-for="day in calendar">
+                <p>Recipe Here: {{}}</p>
+
+            </div>
+
+        </div>
+    </div>
+        
+
+</template>
+<div>
+
+
+    <!--  <div id="sidebar">
+              
+                 <div class="sidebar-header row">
+                    <button type="button" id="sidebarCollapse" class="btn btn-info navbar-btn">
+                        <i class="glyphicon glyphicon-align-right"></i>
+
+                    </button>
+
+
+                                             <ul class="list-unstyled components">
                         <li class="active">
                             <a href="#">
                                 <div class="row">
                                     <div class="col-xs-2">
                                         <div class="row" style="background-color:rgb(202, 86, 86)">
-                                            <strong>Day</strong>
+                                            <strong>{{day.recipe}}</strong>
                                         </div>
                                         <h5>1</h5>
                                     </div>
@@ -84,7 +97,7 @@
                                         <h5>5</h5>
                                     </div>
                                     <div class="col-xs-9" droppable="true" v-on:drop.capture="addItem" ondragover="event.preventDefault()">
-                                        <h4>Dinner here</h4>
+                                        <h4>{{recipe.label}}</h4>
                                     </div>
                                 </div>
                             </a>
@@ -122,92 +135,107 @@
                     </ul>
                 </div>
     
-                <!-- Page Content -->
-                <div id="content">
-                    <!-- We'll fill this with dummy content -->
+                    </div>
+
                 </div>
-    
             </div>
         </div>
+    </div>
+ -->
+
     
-    </template>
-    
-    
+
+
+    <!-- <script>
+    $(document).ready(function () {
+
+        $('#sidebarCollapse').on('click', function () {
+            $('#sidebar').toggleClass('active');
+
+        });
+
+
+    });
+    $(document).ready(function () {
+
+        $('#sidebarCollapse2').on('click', function () {
+            $('#sidebar').toggleClass('active');
+
+
+        });
+ 
+
+    });-->
     <script>
-        $(document).ready(function () {
-    
-            $('#sidebarCollapse').on('click', function () {
-                $('#sidebar').toggleClass('active');
-                
-            });
-            
-    
-        });
-        $(document).ready(function () {
-    
-            $('#sidebarCollapse2').on('click', function () {
-                $('#sidebar').toggleClass('active');
-                
-    
-            });
-            
-    
-        });
-            export default {
+        import cookbook from '../components/cookBook'
+        import recipe from '../components/results'
+
+        export default {
             name: 'calendar',
             data() {
-                return {}
+                return {
+                    calendar: "",
+                    day: ""
+                }
             },
-            computed: {},
-            methods: {
-                addItem(event){
-          var item = JSON.parse(event.dataTransfer.getData('text/javascript'))
-          this.room.items.push(item)
-        }
+            computed: {
+                calendar(){
+                    return this.$store.state.calendar
+                }
             },
-            components: {}
+            /* methods: {
+                addItem(event) {
+                    var item = JSON.parse(event.dataTransfer.getData('text/javascript'))
+                    this.room.items.push(item)
+                }
+            }, */
+            components: {
+               cookBook,
+               results
+            }
         }
     </script>
-    
-    
+
+
     <style scoped>
         .wrapper {
             display: flex;
             align-items: stretch;
-            
-        }
-        
 
-    
+        }
+
+
+
         #sidebar.active {
             margin-left: 223px;
             /* float: right; */
         }
-    
+
         #sidebar {
             min-width: 250px;
             max-width: 250px;
             min-height: 100vh;
             background-color: silver;
         }
-    
+
         .col-xs-2 {
             outline-style: solid;
             background-color: #fef1da;
-    
+
         }
-    
+
         .col-xs-9 {
             outline-style: solid;
             background-color: #fef1da;
-    
+
         }
-    
+
         .h1 {
             font-size: 30px;
             font-size: 3.5vw;
         }
-        .navbar-btn{
+
+        .navbar-btn {
             float: left;
         }
     </style>
