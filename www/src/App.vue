43472014379217a7de90f500be5a4d4bfdd0db07
Bryce Murray <brycepmurray711@gmail.com>
2017-12-14 14:17:56 -0700

    fixed app.vue, and deleted calendar script

--- www/src/App.vue
+++ www/src/App.vue
@@ -1,59 +1,45 @@
 <template>
-  <div class="container">
+  <div class="container-fluid">
     <div id="app">
       <navbar></navbar>
       <router-view></router-view>
     </div>
-
-    <footer>
-      <h6>Coolio, Julio</h6>
-    </footer>
   </div>
 </template>
 
 <script>
-  import Error from './components/Error'
-  import Navbar from './components/Navbar'
+    import Error from './components/Error'
+    import Navbar from './components/Navbar'
 
 
-  export default {
-    name: 'app',
-    components: {
-      Error,
-      Navbar,
-      
-    },
-    methods: {},
 
-   
-    mounted() {
-     this.$store.dispatch('authenticate')
+    export default {
+        name: 'app',
+        components: {
+            Error,
+            Navbar,
+
+        },
+        methods: {},
+
+
+        mounted() {
+            this.$store.dispatch('authenticate')
+        }
     }
-  }
-
 </script>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    background-image: url('http://www.myfreetextures.com/wp-content/uploads/2014/10/seamless-wood-texture-2.jpg')
-  }
-
-button{
-  float: right
-}
-
-  footer {
-    height: 10vh;
-    width: 100%;
-    position: fixed;
-    bottom: 0px;
-    left: 0px;
-    background-color: #333;
-    color: white
-  }
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        background-image: url('http://www.myfreetextures.com/wp-content/uploads/2014/10/seamless-wood-texture-2.jpg')
+    }
+    
+    button {
+        float: right
+    }
 </style>
