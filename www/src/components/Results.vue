b08c0f7a136e9c845a934d1caac18cd992b7ccb2
Bryce Murray <brycepmurray711@gmail.com>
2017-12-15 15:20:21 -0700

    more spacing!

--- www/src/components/Results.vue
+++ www/src/components/Results.vue
@@ -192,14 +192,14 @@
         background-color: rgb(255, 255, 255);
         font-size: 2rem;
         height: 3rem;
-        margin-top: 10px;
+        margin-top: 20px;
         margin-left: 10%
     }
     
     .submit {
         height: 3rem;
         margin-right: 40%;
-        margin-top: 10px;
+        margin-top: 20px;
     }
     
     .glyphicon:hover {
