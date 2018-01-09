4b7035b0279770e9c4f01e7a514ce710c1d86d99
Bryce Murray <brycepmurray711@gmail.com>
2018-01-09 15:26:28 -0700

    card style back

--- www/src/components/CookBook.vue
+++ www/src/components/CookBook.vue
@@ -60,13 +60,7 @@
             </div>
         </div>
         <!-- end of Modal -->
-
-        <div class="row text-left " id="main">
-
-                <div v-for="meal in cookBook">
-                        <Meal :mealprop="meal"></Meal>
-                    </div>
-
+        <div class="row text-center" id="main">
             <div >
                 <button id="openNav" class="w3-button w3-teal w3-xlarge" @click="openCal()"><span class="glyphicon glyphicon-calendar"></span></button>
             </div>
@@ -75,12 +69,7 @@
                 <div class="col-lg-4 col-xs-4 fixedYo ">
                     <span @click="closeCal()" class="w3-bar-item w3-button w3-large glyphicon glyphicon-remove"></span>
                     <table class="table-fill">
-                        <thead>
-                            <tr>
-                                <th class="day" >Week 1</th>
-                                
-                            </tr>
-                        </thead>
+                       
                         <tbody class="table-hover">
                             
                                 <!-- <td class="day">Monday</td>
@@ -97,6 +86,12 @@
 
 
             </div>
+            
+            <div v-for="meal in cookBook">
+                <Meal :mealprop="meal"></Meal>
+            </div>
+            
+            
         </div>
     </div>
 </template>
@@ -148,7 +143,7 @@
             },
 
             openCal() {
-                document.getElementById("main").style.marginRight = "25%";
+                document.getElementById("main").style.marginRight = "48%";
                 document.getElementById("mySidebar").style.width = "40%";
                 document.getElementById("mySidebar").style.height = "70%";
                 document.getElementById("mySidebar").style.display = "block";
@@ -169,6 +164,8 @@
     }
 </script>
 <style scoped>
+    /* Card styling Start */
+    
     #openNav {
         position: fixed;
         width: 75px;
@@ -269,7 +266,7 @@
     .fixedYo {
         position: fixed;
         right: 80px;
-        top: 88px;
+        top: 50px;
     }
     
     .bryce {
--- www/src/components/Meal.vue
+++ www/src/components/Meal.vue
@@ -63,4 +63,77 @@
 
 
 <style scoped>
+    .container-fluid {
+        text-align: center;
+        width: 80%;
+    }
+    
+    .words {
+        font-size: 2rem
+    }
+    
+    .glyphicon-new-window {
+        margin-left: 10px
+    }
+    
+    .hashtag {
+        padding: 40px;
+        text-align: center;
+    }
+    
+    .modal-title {
+        background-color: rgba(0, 0, 0, 0.603);
+        color: white
+    }
+    
+    .modal-body {
+        text-align: left;
+        background-color: rgba(75, 75, 75, 0.726);
+        color: white
+    }
+    
+    .row {
+        width: 100%
+    }
+    
+    .wide {
+        width: 100%
+    }
+    
+    .card {
+        padding: 15px;
+        margin: 25px;
+        height: 400px;
+        background-color: rgba(255, 255, 255, 0.700);
+        box-shadow: 5px 5px rgb(138, 138, 138);
+        border-radius: 6%
+    }
+    
+    .card:hover {
+        cursor: pointer;
+        background-color: white;
+        box-shadow: 5px 5px rgb(44, 44, 44);
+    }
+    
+    .card-img-top {
+        width: 200px;
+        height: 200px;
+        border-radius: 6%
+    }
+    
+    .glyphicon-trash:hover {
+        transform: scale(2, 2);
+    }
+    
+    .glyphicon-trash:hover {
+        color: rgb(150, 1, 1)
+    }
+    
+    .ft {
+        text-align: right;
+    }
+    
+    .to {
+        margin-top: 10px;
+    }
 </style>
